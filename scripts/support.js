4b73a135197e51167104f47e39ae2bd85e82db08
Akinayodeji <akinayodeji4all@gmail.com>
2017-09-08 01:31:35 +0100

    Update support.js

--- scripts/support.js
+++ scripts/support.js
@@ -12,26 +12,5 @@
 
      }
    }
-   ///--------
-  
-    var el = document.createElement("div").style;
-    if(('flexWrap' in el) || ('WebkitFlexWrap' in el) || ('msFlexWrap' in el)){
-          $eachElem(".flex", function(index, elem){
-               elem.classList.add("flex");
-           });
-    }else{
-          $eachElem(".flex", function(index, elem){
-               elem.classList.remove("flex");
-           });
-    }
-
-    //-------fallback------
-   flex = CSS.supports("display", "flex");
-   flexWrap = CSS.supports("flex-wrap", "wrap");
-    if(!flex || !flexWrap){
-
-        $eachElem(".flex", function(index, elem){
-            elem.classList.remove("flex");
-         });
-    }
+   
 }())
